0711d4d78f760e8d6b1acd8d184db217fff03718
Hugo Cowan <hugocowan@hotmail.co.uk>
2018-05-04 15:05:31 +0100

    Added gAutocomplete and dependencies for app.js

---
+++ src/directives/gMap.js
@@ -0,0 +1,27 @@
+/*global google*/
+function gMap(){
+  return {
+    restrict: 'A',
+    scope: {
+      center: '='
+    },
+    link($scope, $element){
+
+      const map = new google.maps.Map($element[0], {
+        center: { lat: 51.515, lng: -0.078 },
+        zoom: 14
+      });
+
+      const marker = new google.maps.Marker({
+        map: map,
+        position: map.getCenter()
+      });
+
+      $scope.$watch('center', () => {
+        map.setCenter($scope.center);
+        marker.setPosition($scope.center);
+      });
+    }
+  };
+}
+export default gMap;
