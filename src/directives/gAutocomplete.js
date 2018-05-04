4e968d088ff64a918bb2b01a92cf3146b53c866e
CarolineR94 <caroline.roden94@gmail.com>
2018-05-04 15:03:05 +0100

    gmaps autocomplete added to directives

---
+++ src/directives/gAutocomplete.js
@@ -0,0 +1,19 @@
+/* global google */
+
+function gAutocomplete(){
+  return {
+    restrict: 'A',
+    scope: {
+      handleChange: '&'
+    },
+    link($scope, $element){
+      console.log($element);
+      const autocomplete = new google.maps.places.Autocomplete($element[0]);
+      autocomplete.addListener('place_changed', () => {
+        $scope.handleChange({ location: autocomplete.getPlace().geometry.location.toJSON()});
+      });
+    }
+  };
+}
+
+export default gAutocomplete;
