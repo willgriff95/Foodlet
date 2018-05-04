a6ed91ee4231bde9a78d73bb17e313901b6edfb1
Hugo Cowan <hugocowan@hotmail.co.uk>
2018-05-04 15:38:05 +0100

    Added moment and time created to food model.

--- models/food.js
+++ models/food.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const moment = require('moment');
 
 const foodSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.ObjectId, ref: 'User' },
@@ -13,6 +14,20 @@ const foodSchema = new mongoose.Schema({
 
 },{
   timestamps: true
+});
+
+foodSchema.virtual('createdAtRelative')
+  .get(function(){
+    return moment(this.createdAt).fromNow();
+  });
+
+foodSchema.set('toJSON', {
+  virtuals: true,
+  transform(doc, json){
+    delete json.createdAt;
+    delete json.updatedAt;
+    return json;
+  }
 });
 
 module.exports = mongoose.model('Food', foodSchema);
