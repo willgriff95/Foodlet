d2977293717372e9604a3ca99a3ef9e97e76486a
Hugo Cowan <hugocowan@hotmail.co.uk>
2018-05-04 09:45:26 +0100

    Added backend index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const router = require('./config/router');
+const { port, dbURI } = require('./config/environment');
+const errorHandler = require('./lib/errorHandler');
+
+mongoose.connect(dbURI);
+
+app.use(bodyParser.json());
+app.use('/api', router);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Up and running on port ${port}`));
+
+module.exports = app;
