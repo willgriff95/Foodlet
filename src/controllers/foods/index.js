ad12f3af2d91d8246d40e60e248eb8f5dbf9668e
Hugo Cowan <hugocowan@hotmail.co.uk>
2018-05-05 14:18:04 +0100

    added userShow to the index controller, edited the view index.

--- src/controllers/foods/index.js
+++ src/controllers/foods/index.js
@@ -1,10 +1,18 @@
-FoodsIndexCtrl.$inject = ['Food'];
+FoodsIndexCtrl.$inject = ['Food', '$http'];
 
 function FoodsIndexCtrl(Food){
   this.all = [];
 
   Food.find()
     .then(res => this.all = res.data);
+
+  function showUser(id, $http){
+    this.user = {};
+    $http
+      .get(`/api/users/${id}`)
+      .then(res => this.user = res.data);
+  }
+  this.showUser = showUser;
 }
 
 export default FoodsIndexCtrl;
