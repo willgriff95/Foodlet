a6696ff55258fe06636034ae2d358a5e667668a6
Hugo Cowan <hugocowan@hotmail.co.uk>
2018-05-05 14:54:51 +0100

    Added code to save the logged in user's data in the object currentUser, located in login.js.

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -1,12 +1,25 @@
-LoginCtrl.$inject = ['$auth', '$state'];
+LoginCtrl.$inject = ['$auth', '$state', '$http'];
 //$auth refers to a package called satellizer.
-function LoginCtrl($auth, $state){
+function LoginCtrl($auth, $state, $http){
   this.data = {};
+  this.currentUser = {};
 
   function handleLogin(){
     if(this.form.$invalid) return false;
+    const vm = this.data;
 
     $auth.login(this.data)
+      .then(() => {
+        $http
+          .get('/api/users')
+          .then(res => {
+            res.forEach(function(user){
+              if(user.username === vm.username){
+                this.currentUser = user;
+              }
+            });
+          });
+      })
       .then(()=> $state.go('foodsIndex'));
   }
 
