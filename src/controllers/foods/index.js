7ae6c1d7e2b54eae68c2e2c973b56675d46de5d4
Hugo Cowan <hugocowan@hotmail.co.uk>
2018-05-06 19:18:00 +0100

    Added  to foods index controller

--- src/controllers/foods/index.js
+++ src/controllers/foods/index.js
@@ -1,12 +1,19 @@
-FoodsIndexCtrl.$inject = ['Food', '$http'];
+/*global google*/
+FoodsIndexCtrl.$inject = ['Food', '$http', '$rootScope'];
 
-function FoodsIndexCtrl(Food){
+function FoodsIndexCtrl(Food, $rootScope){
   this.all = [];
-  this.location = [];
 
   Food.find()
+    .then(() => $rootScope.location = [])
     .then(res => res.data.forEach(food => {
       this.location.push(food.location);
+    }))
+    //Trying to add map markers for each food location.
+    .then(() => this.location.forEach(location => {
+      return new google.maps.Marker({
+        position: new google.maps.LatLng(location.lat, location.lng)
+      });
     }))
     .then(res => this.all = res.data);
 }
