214e6cfa2fa192f8ff4e550bc2d46c9dd79deac5
Hugo Cowan <hugocowan@hotmail.co.uk>
2018-05-06 11:07:21 +0100

    Got all food locations into an array.

--- src/controllers/foods/index.js
+++ src/controllers/foods/index.js
@@ -2,8 +2,12 @@ FoodsIndexCtrl.$inject = ['Food', '$http'];
 
 function FoodsIndexCtrl(Food){
   this.all = [];
+  this.location = [];
 
   Food.find()
+    .then(res => res.data.forEach(food => {
+      this.location.push(food.location);
+    }))
     .then(res => this.all = res.data);
 }
 
