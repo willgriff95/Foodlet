935d45ff235b87b0fe5a224c1ba82c4740b924b2
Hugo Cowan <hugocowan@hotmail.co.uk>
2018-05-08 10:08:38 +0100

    Mike came to help. currentUser remains defined on page refresh, and is removed on logout. Index page google maps remains even after refresh. Each food listing has a card in the index's google maps.

--- src/directives/gMapIndex.js
+++ src/directives/gMapIndex.js
@@ -1,198 +1,85 @@
 /*global google*/
-gMapIndex.$inject = ['Food'];
-
-function gMapIndex(Food){
+function gMapIndex(){
   return {
     restrict: 'A',
     scope: {
-      center: '='
+      foods: '=',
+      center: '=?'
     },
 
     link($scope, $element){
+      console.log($element);
       const map = new google.maps.Map($element[0], {
         center: { lat: 51.515, lng: -0.078 },
-        zoom: 14,
-        scrollwheel: true,
-        disableDefaultUI: true,
-        styles: [{
-          'featureType': 'water',
-          'elementType': 'geometry',
-          'stylers': [
-            {
-              'color': '#dcf2ff'
-            },
-            {
-              'lightness': 17
-            }
-          ]
-        },
-        {
-          'featureType': 'landscape',
-          'elementType': 'geometry',
-          'stylers': [
-            {
-              'color': '#f5f5f5'
-            },
-            {
-              'lightness': 20
-            }
-          ]
-        },
-        {
-          'featureType': 'road.highway',
-          'elementType': 'geometry.fill',
-          'stylers': [
-            {
-              'color': '#ffffff'
-            },
-            {
-              'lightness': 17
-            }
-          ]
-        },
-        {
-          'featureType': 'road.highway',
-          'elementType': 'geometry.stroke',
-          'stylers': [
-            {
-              'color': '#ffffff'
-            },
-            {
-              'lightness': 29
-            },
-            {
-              'weight': 0.2
-            }
-          ]
-        },
-        {
-          'featureType': 'road.arterial',
-          'elementType': 'geometry',
-          'stylers': [
-            {
-              'color': '#ffffff'
-            },
-            {
-              'lightness': 18
-            }
-          ]
-        },
-        {
-          'featureType': 'road.local',
-          'elementType': 'geometry',
-          'stylers': [
-            {
-              'color': '#ffffff'
-            },
-            {
-              'lightness': 16
-            }
-          ]
-        },
-        {
-          'featureType': 'poi',
-          'elementType': 'geometry',
-          'stylers': [
-            {
-              'color': '#f5f5f5'
-            },
-            {
-              'lightness': 21
-            }
-          ]
-        },
-        {
-          'featureType': 'poi.park',
-          'elementType': 'geometry',
-          'stylers': [
-            {
-              'color': '#e1ffc5'//#99CC33'//#9DDD48
-            },
-            {
-              'lightness': 21
-            }
-          ]
-        },
-        {
-          'elementType': 'labels.text.stroke',
-          'stylers': [
-            {
-              'visibility': 'on'
-            },
-            {
-              'color': '#ffffff'
-            },
-            {
-              'lightness': 16
-            }
-          ]
-        },
-        {
-          'elementType': 'labels.text.fill',
-          'stylers': [
-            {
-              'saturation': 36
-            },
-            {
-              'color': '#333333'
-            },
-            {
-              'lightness': 40
-            }
-          ]
-        },
-        {
-          'elementType': 'labels.icon',
-          'stylers': [
-            {
-              'visibility': 'off'
-            }
-          ]
-        }]
+        zoom: 14
       });
       const marker = new google.maps.Marker({
         map: map,
         position: map.getCenter(),
         icon: 'https://i.imgur.com/aVQgzGW.png?1'
       });
-      const infowindows = [];
-      const foodMarkers = [];
+      const infoWindow = new google.maps.InfoWindow();
+      let foodMarkers = [];
 
       $scope.$watch('center', () => {
+        if(!$scope.center) return false;
         map.setCenter($scope.center);
         marker.setPosition($scope.center);
       });
 
-      // shows all food markers
-      Food
-        .find()
-        .then(res => {
-          res.data.forEach(food => {
-            const title = food.title;
-            const description = food.description;
-            infowindows.push(new google.maps.InfoWindow({
-              content: `<div id="siteNotice" ><p>${title}</p><p>${description}</p></div>`
-            }));
-            foodMarkers.push(new google.maps.Marker({
-              position: new google.maps.LatLng(food.location.lat, food.location.lng),
-              map: map,
-              animation: google.maps.Animation.DROP,
-              title: food.title,
-              icon: 'https://i.imgur.com/aVQgzGW.png?1'
-            }));
+      $scope.$watch('foods', () => {
+        foodMarkers.forEach(marker => marker.setMap(null));
+        foodMarkers = $scope.foods.map(food => {
+          const marker = new google.maps.Marker({
+            position: food.location,
+            map,
+            icon: 'https://i.imgur.com/aVQgzGW.png?1'
           });
-        })
-        .then(() => {
-          console.log(infowindows);
-          console.log(foodMarkers);
-          foodMarkers.forEach(foodMarker => {
-            infowindows.forEach(infowindow => {
-              foodMarker.addListener('click', function() {
-                infowindow.open(map, marker);
-              });
-            });
+
+          marker.addListener('click', () => {
+            infoWindow.setContent(`
+              <div id="siteNotice" >
+                <p>${food.title}</p>
+                <p>${food.description}</p>
+              </div>
+            `);
+            infoWindow.open(map, marker);
           });
+
+          return marker;
         });
+      });
+
+      // shows all food markers
+      // Food
+      //   .find()
+      //   .then(res => {
+      //     res.data.forEach(food => {
+      //       const title = food.title;
+      //       const description = food.description;
+      //       infowindows.push(new google.maps.InfoWindow({
+      //         content: `<div id="siteNotice" ><p>${title}</p><p>${description}</p></div>`
+      //       }));
+      //       foodMarkers.push(new google.maps.Marker({
+      //         position: new google.maps.LatLng(food.location.lat, food.location.lng),
+      //         map: map,
+      //         animation: google.maps.Animation.DROP,
+      //         title: food.title,
+      //         icon: 'https://i.imgur.com/aVQgzGW.png?1'
+      //       }));
+      //     });
+      //   })
+      //   .then(() => {
+      //     console.log(infowindows);
+      //     // console.log(foodMarkers);
+      //     foodMarkers.forEach(foodMarker => {
+      //       infowindows.forEach(infowindow => {
+      //         foodMarker.addListener('click', function() {
+      //           infowindow.open(map, foodMarker);
+      //         });
+      //       });
+      //     });
+      //   });
     }
   };
 }
