5ab9d497393db5473b39b3f5fe510bd1274b01cd
CarolineR94 <caroline.roden94@gmail.com>
2018-05-09 10:45:49 +0100

    requests working, accept button not working

--- src/controllers/foods/show.js
+++ src/controllers/foods/show.js
@@ -21,9 +21,23 @@ function FoodsShowCtrl(Food, $state){
       .then(res => this.food = res.data);
   }
 
+  function handleRequestAccept(){
+    this.request = {status: 'accepted'};
+    Food
+      .findById($state.params.requestId)
+      .then(res => {
+        this.request = res.data;
+      })
+      .then(res => {
+        Food
+          .requestAccept($state.params.id, $state.params.requestId, res.data);
+      });
+  }
+
 
   this.handleDelete = handleDelete;
   this.handleRequestCreate = handleRequestCreate;
+  this.handleRequestAccept = handleRequestAccept;
 
 }
 
