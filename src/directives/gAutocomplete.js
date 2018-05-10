ca833110d6d6b13d0cb2dd56265e9d709898f6bf
Hugo Cowan <hugocowan@hotmail.co.uk>
2018-05-10 21:52:46 +0100

    Silenced all console logs

--- src/directives/gAutocomplete.js
+++ src/directives/gAutocomplete.js
@@ -7,7 +7,7 @@ function gAutocomplete(){
       handleChange: '&'
     },
     link($scope, $element){
-      console.log($element);
+      // console.log($element);
       const autocomplete = new google.maps.places.Autocomplete($element[0]);
       autocomplete.addListener('place_changed', () => {
         $scope.handleChange({ location: autocomplete.getPlace().geometry.location.toJSON()});
