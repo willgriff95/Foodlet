34fbf258b9bbb83fcb606dfa33ee2c26701c05d8
CarolineR94 <caroline.roden94@gmail.com>
2018-05-11 11:40:23 +0100

    video added

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,8 @@ module.exports = {
       { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' },
       { test: /\.jpe?g(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/jpeg' },
       { test: /\.gif(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/gif' },
-      { test: /\.png(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/png' }
+      { test: /\.png(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/png' },
+      { test: /\.webm(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=video/webm' }
     ]
   },
   devServer: {
