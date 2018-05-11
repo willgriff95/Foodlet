26a9e9ac95e88f221ddfe1ecea4e1c791f0efc7b
CarolineR94 <caroline.roden94@gmail.com>
2018-05-11 11:24:55 +0100

    seeds added

--- db/seeds.js
+++ db/seeds.js
@@ -81,7 +81,7 @@ mongoose.connect(dbURI, (err, db) => {
           description: 'Unopened bag of wholewheat pasta',
           location: {
             lat: 51.5,
-            lng: -0.02
+            lng: -0.09
           },
           image: 'https://img.tesco.com/Groceries/pi/305/5000119532305/IDShot_540x540.jpg',
           active: true,
@@ -90,8 +90,8 @@ mongoose.connect(dbURI, (err, db) => {
           title: 'Apricots',
           description: 'Fresh apricots from my garden, I\'ve grown too many!',
           location: {
-            lat: 51.008,
-            lng: -0.03
+            lat: 51.208,
+            lng: -0.02
           },
           image: 'http://www.slowburningpassion.com/wp-content/uploads/2016/07/Bowl-of-ripe-apricots.jpg',
           active: true,
@@ -100,12 +100,42 @@ mongoose.connect(dbURI, (err, db) => {
           title: 'Olives',
           description: '1kg jar of Greek olives',
           location: {
-            lat: 51.118,
+            lat: 51.318,
             lng: -0.06
           },
           image: 'https://cdn.shopify.com/s/files/1/0206/9470/products/60470_HFARM_9332683000024-1_500d4751-a5d0-458b-900c-3d7d83390fca_1024x1024.jpeg?v=1441108615',
           active: true,
           user: users[1]
+        }, {
+          title: 'Jar of peanut butter',
+          description: 'Leftover jar of peanut butter which I don\'t like',
+          location: {
+            lat: 51.412,
+            lng: -0.075
+          },
+          image: 'https://www.telegraph.co.uk/content/dam/food-and-drink/2018/01/19/5052449669527_T1_trans_NvBQzQNjv4Bqeo_i_u9APj8RuoebjoAHt0k9u7HhRJvuo-ZLenGRumA.jpg?imwidth=480',
+          active: true,
+          user: users[0]
+        }, {
+          title: 'Strawberries',
+          description: 'From my strawberry farm',
+          location: {
+            lat: 52.6369,
+            lng: 1.1398
+          },
+          image: 'https://korea.stripes.com/sites/korea.stripes.com/files/u1814/image_1_0.jpg',
+          active: true,
+          user: users[1]
+        }, {
+          title: 'Bananas',
+          description: 'From my banana farm',
+          location: {
+            lat: 50.1934,
+            lng: 1.8931
+          },
+          image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQorx0dT_iqiL-PUcM-bNlWIB0953vocqX1I9GtnDAZEKyfatxn',
+          active: true,
+          user: users[2]
         }
 
       ]);
